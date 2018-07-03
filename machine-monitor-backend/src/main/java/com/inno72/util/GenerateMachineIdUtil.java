6ef92a58f05ebce246cbe52c459d75716e3c97d5
72cy-0101-01-0013 <wangxiuting@inno72.com>
2018-07-03 11:59:00 +0800

    随机生成10位机器编码工具类-wxt

---
+++ machine-monitor-backend/src/main/java/com/inno72/util/GenerateMachineIdUtil.java
@@ -0,0 +1,35 @@
+package com.inno72.util;
+
+import java.util.Random;
+
+/**
+ * @Auther: wxt
+ * @Date: 2018/7/3 10:49
+ * @Description:随机生成机器Id
+ */
+public class GenerateMachineIdUtil {
+
+        private static final String NUMBER_CHAR = "0123456789";
+
+        public static String generateNumberString(int length){
+            StringBuffer sb = new StringBuffer();
+            Random random = new Random();
+            for (int i = 0; i < length; i++) {
+                sb.append(NUMBER_CHAR.charAt(random.nextInt(NUMBER_CHAR.length())));
+            }
+            return sb.toString();
+        }
+
+
+
+    public static void main(String[] args) throws Exception {
+        for(int i = 0;i <= 2000;i++){
+           String time = generateNumberString(10);
+
+            System.out.println(" " + time);
+        }
+
+    }
+
+}
+
