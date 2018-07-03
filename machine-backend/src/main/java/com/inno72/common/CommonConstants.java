80eaea94ac2f524227635201a25d74f8d8f35c28
lzh <lizonghao@inno72.com>
2018-07-03 18:53:22 +0800

    钉钉同步

--- machine-backend/src/main/java/com/inno72/common/CommonConstants.java
+++ machine-backend/src/main/java/com/inno72/common/CommonConstants.java
@@ -2,4 +2,8 @@ package com.inno72.common;
 
 public class CommonConstants {
 
+	public final static String DD_TOKEN = "123456";
+	public final static String AES_KEY = "1111111111111111111111111111111111111111111";
+	public final static String DD_CORPID = "dingd04d2d6ca18d0fd535c2f4657eb6378f";
+
 }
--- machine-backend/src/test/java/com/inno72/generator/CodeGenerator.java
+++ machine-backend/src/test/java/com/inno72/generator/CodeGenerator.java
@@ -60,7 +60,7 @@ public class CodeGenerator {
 	private static final String DATE = new SimpleDateFormat("yyyy/MM/dd").format(new Date());// @date
 
 	public static void main(String[] args) {
-		genCode("inno72_channel");
+		genCode("inno72_user");
 		// genCodeByCustomModelName("输入表名","输入自定义Model名称");
 	}
 
