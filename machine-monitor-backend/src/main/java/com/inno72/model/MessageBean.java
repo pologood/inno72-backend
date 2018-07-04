8defd08616aa68c04914a66b07a7b9255510f626
lzh <lizonghao@inno72.com>
2018-07-04 17:12:26 +0800

    修改

---
+++ machine-monitor-backend/src/main/java/com/inno72/model/MessageBean.java
@@ -0,0 +1,59 @@
+package com.inno72.model;
+
+public class MessageBean<T> {
+
+	private Integer eventType;
+	private Integer subEventType;
+	private T t;
+
+	public enum EventType {
+		// 查看状态
+		CHECKSTATUS(1),
+		// 更新App
+		UPDATEAPP(2);
+
+		private int v;
+
+		private EventType(int v) {
+			this.v = v;
+		}
+
+		public int v() {
+			return this.v;
+		}
+
+		public static EventType get(int v) {
+			for (EventType c : EventType.values()) {
+				if (c.v == v) {
+					return c;
+				}
+			}
+			return null;
+		}
+	}
+
+	public Integer getEventType() {
+		return eventType;
+	}
+
+	public void setEventType(Integer eventType) {
+		this.eventType = eventType;
+	}
+
+	public Integer getSubEventType() {
+		return subEventType;
+	}
+
+	public void setSubEventType(Integer subEventType) {
+		this.subEventType = subEventType;
+	}
+
+	public T getT() {
+		return t;
+	}
+
+	public void setT(T t) {
+		this.t = t;
+	}
+
+}
--- machine-monitor-backend/src/main/java/com/inno72/socketio/SocketIOStartHandler.java
+++ machine-monitor-backend/src/main/java/com/inno72/socketio/SocketIOStartHandler.java
@@ -1,9 +1,10 @@
 package com.inno72.socketio;
 
-import com.inno72.plugin.http.HttpClient;
-import com.inno72.redis.IRedisUtil;
-import com.inno72.socketio.core.SocketServer;
-import com.inno72.socketio.core.SocketServerHandler;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +12,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoOperations;
 
-import javax.annotation.Resource;
-import java.util.List;
-import java.util.Map;
+import com.alibaba.fastjson.JSON;
+import com.inno72.model.MessageBean;
+import com.inno72.plugin.http.HttpClient;
+import com.inno72.redis.IRedisUtil;
+import com.inno72.socketio.core.SocketServer;
+import com.inno72.socketio.core.SocketServerHandler;
 
 @Configuration
 public class SocketIOStartHandler {
@@ -34,33 +38,35 @@ public class SocketIOStartHandler {
 
 				log.info("获取机器Id方法开始,sessionId=" + key + ",deviceId" + param);
 
-				//变量
+				// 变量
 				String machineId;
 
-				//根据从缓存中取机器Id，取不到从数据库中取
-				if(redisUtil.exists(key) == false){
-					//根据deviceId获取机器Id
-					machineId = HttpClient.post("http://localhost:8880/machine/generateMachineId?deviceId=1","");
-					//存入redis中
-					redisUtil.set(key,machineId);
-				}else{
+				// 根据从缓存中取机器Id，取不到从数据库中取
+				if (redisUtil.exists(key) == false) {
+					// 根据deviceId获取机器Id
+					machineId = HttpClient.post("http://localhost:8880/machine/generateMachineId?deviceId=1", "");
+					// 存入redis中
+					redisUtil.set(key, machineId);
+				} else {
 					machineId = redisUtil.get(key);
 				}
 
 				log.info("获取机器Id方法结束,sessionId=" + key + ",machineId=" + machineId + ",deviceId" + param);
 
+				MessageBean ms = JSON.parseObject(data, MessageBean.class);
+				if (ms.getEventType() == MessageBean.EventType.CHECKSTATUS.v()) {
+				}
 				return machineId;
 			}
 
 			@Override
 			public void monitorResponse(String key, String data, Map<String, List<String>> params) {
 				log.info("推送监控消息方法执行开始" + data);
-				//解压缩以及解密数据
+				// 解压缩以及解密数据
 
+				// 解析数据
 
-				//解析数据
-
-				//存入mongoDB数据库中
+				// 存入mongoDB数据库中
 
 				log.info("推送监控消息方法执行结束" + data);
 			}
@@ -82,10 +88,10 @@ public class SocketIOStartHandler {
 	public SocketServer socketServer() {
 		return new SocketServer("0.0.0.0", 1237, socketServerHandler());
 	}
+
 	public static void main(String[] args) throws Exception {
-		String string = HttpClient.post("http://172.16.18.240:8880/machine/generateMachineId?id=1","");
+		String string = HttpClient.post("http://172.16.18.240:8880/machine/generateMachineId?id=1", "");
 		System.out.println("我的返回值是" + string);
 	}
-
 
 }
