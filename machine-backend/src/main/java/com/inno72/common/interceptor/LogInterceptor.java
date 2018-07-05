22261db160fad9bf8708e6ea9d61c4f359437928
赵飞龙 <zhaofeilong@inno72.com>
2018-07-05 19:04:47 +0800

    修改拦截器过滤接口

--- machine-backend/src/main/java/com/inno72/common/interceptor/LogInterceptor.java
+++ machine-backend/src/main/java/com/inno72/common/interceptor/LogInterceptor.java
@@ -34,7 +34,8 @@ public class LogInterceptor extends HandlerInterceptorAdapter {
 	private IRedisUtil redisUtil; // memcachedClient
 
 	private static List<String> doNotCheckUs = Arrays.asList(new String[] { "/dd/login", "/dd", "/dd/reg_call_back",
-			"/dd/update_call_back", "/dd/initDData", "/dd/token", "/dd/testLogin" });
+			"/dd/update_call_back", "/dd/initDData", "/dd/token", "/dd/testLogin","/machine/channel/out/subCount",
+	"/machine/channel/out/get"});
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
