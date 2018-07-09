08edf2a152b0b853e652bd91dbd6d868687828c2
72CY-0101-01-0014 <zhangwenjie@inno72.com>
2018-07-09 09:55:11 +0800

    设置机器

--- machine-backend/src/main/java/com/inno72/game/mapper/Inno72GameMapper.java
+++ machine-backend/src/main/java/com/inno72/game/mapper/Inno72GameMapper.java
@@ -3,6 +3,8 @@ package com.inno72.game.mapper;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.inno72.common.Mapper;
 import com.inno72.game.model.Inno72Game;
 
@@ -13,6 +15,11 @@ public interface Inno72GameMapper extends Mapper<Inno72Game> {
 	
 	int selectIsUseing(String id);
 	List<Inno72Game> selectByPage(Map<String, Object> params);
+	
+	void deleteMachineGameByMachineId(@Param("mIds") List<String> mIds);
+	
+	
+	void addMachineGame(@Param("machineGames") List<Map<String, Object>> machineGames);
 	
 	
 }
