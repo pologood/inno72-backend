3ef5dc0706737408c22eceeb779064fc5d842951
72cy-0101-01-0013 <wangxiuting@inno72.com>
2018-07-13 11:56:57 +0800

    新增修改网络状态方法提交 -wxt

--- machine-backend/src/main/java/com/inno72/machine/service/MachineService.java
+++ machine-backend/src/main/java/com/inno72/machine/service/MachineService.java
@@ -4,6 +4,8 @@ import com.inno72.common.Result;
 import com.inno72.common.Service;
 import com.inno72.machine.model.Inno72Machine;
 
+import java.util.List;
+
 /**
  * Created by CodeGenerator on 2018/06/29.
  */
@@ -12,5 +14,9 @@ public interface MachineService extends Service<Inno72Machine> {
 	Result<String> initMachine(String deviceId, String channelJson);
 
 	Result<String> updateNetStatus(String machineCode, Integer netStatus);
+
+	Result<List<String>> updateMachineListNetStatus(List<String> list, Integer netStatus);
+
+
 
 }
