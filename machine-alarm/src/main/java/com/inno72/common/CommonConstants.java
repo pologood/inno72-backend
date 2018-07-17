1f53294be2a2ef6623d287ba17849ef49e384a4a
72cy-0101-01-0013 <wangxiuting@inno72.com>
2018-07-17 15:09:49 +0800

    常量类提交 -wxt

--- machine-alarm/src/main/java/com/inno72/common/CommonConstants.java
+++ machine-alarm/src/main/java/com/inno72/common/CommonConstants.java
@@ -2,8 +2,8 @@ package com.inno72.common;
 
 public class CommonConstants {
 
+    public final static String MACHINE_EXCEPTION = "machineException";
 
-
-
+    public final static Integer MACHINE_DOOR_OPEN = 1;
 
 }
