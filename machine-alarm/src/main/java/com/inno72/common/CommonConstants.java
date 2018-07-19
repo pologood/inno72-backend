3fd337929daf9cfc3029ddfe11d785f76c6dcd5c
72cy-0101-01-0013 <wangxiuting@inno72.com>
2018-07-19 15:16:34 +0800

    提交常量 -wxt

--- machine-alarm/src/main/java/com/inno72/common/CommonConstants.java
+++ machine-alarm/src/main/java/com/inno72/common/CommonConstants.java
@@ -6,20 +6,16 @@ public class CommonConstants {
 
     public final static String MACHINE_CHANNEL_EXCEPTION = "machineChannelException";
 
-    public final static String MACHINE_LACK_GOODS = "machineLackGoods";
+    public final static String MACHINE_LACKGOODS_EXCEPTION = "machineLackGoodsException";
 
-    public final static Integer MACHINE_DOOR_OPEN = 1;
+    public final static String SYS_MACHINE_CHANNEL = "machineChannel";
+
+    public final static String SYS_MACHINE_DROPGOODS = "machineDropGoods";
+
+    public final static String SYS_MACHINE_LACKGOODS = "machineLackGoods";
 
     public final static Integer BETWEEN_FIVE = 5;
 
     public final static Integer BETWEEN_EIGHT = 8;
-
-    public final static String XUNJIAN_APP = "xunJianApp";
-
-    public final static String DOUBLE_ALARM = "doubleAlarm";
-
-    public final static String MESSAGE = "message";
-
-    public final static String DINGDING_APP = "dingDingApp";
 
 }
