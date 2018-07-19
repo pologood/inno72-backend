6ce20b44dfcef53cb99bd68a71a165a637c18559
72CY-0101-01-0014 <zhangwenjie@inno72.com>
2018-07-19 16:27:44 +0800

    ‘故障单列表’

--- machine-backend/src/main/java/com/inno72/check/mapper/Inno72CheckFaultMapper.java
+++ machine-backend/src/main/java/com/inno72/check/mapper/Inno72CheckFaultMapper.java
@@ -1,10 +1,15 @@
 package com.inno72.check.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import com.inno72.check.model.Inno72CheckFault;
 import com.inno72.common.Mapper;
 
 @org.apache.ibatis.annotations.Mapper
 public interface Inno72CheckFaultMapper extends Mapper<Inno72CheckFault> {
 	
+	
+	List<Inno72CheckFault> selectByPage(Map<String, Object> params);
 	
 }
--- machine-backend/src/main/java/com/inno72/check/service/CheckFaultService.java
+++ machine-backend/src/main/java/com/inno72/check/service/CheckFaultService.java
@@ -1,11 +1,18 @@
 package com.inno72.check.service;
 
+import java.util.List;
+
 import com.inno72.check.model.Inno72CheckFault;
+import com.inno72.common.Result;
 import com.inno72.common.Service;
 
 /**
  * Created by CodeGenerator on 2018/07/19.
  */
 public interface CheckFaultService extends Service<Inno72CheckFault> {
+
+	List<Inno72CheckFault> findByPage(String keyword, String status, String type, String startTime, String endTime);
+
+	Result<String> faultAnswer(String id, String remark);
 
 }
