b253f0bab7b211bf452d98b3ed0df6444f464092
lzh <lizonghao@inno72.com>
2018-07-19 13:52:53 +0800

    修改名字

--- machine-check-app-backend/src/main/java/com/MachineCheckAppBackendApplication.java
+++ machine-check-app-backend/src/main/java/com/MachineCheckAppBackendApplication.java
@@ -1,27 +1,28 @@
 package com;
 
-import com.inno72.common.MachineAppBackendProperties;
-import com.inno72.springboot.web.SpringApplicationBuilder;
-import com.inno72.springboot.web.SpringBootServletInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+import com.inno72.common.MachineAppBackendProperties;
+import com.inno72.springboot.web.SpringApplicationBuilder;
+import com.inno72.springboot.web.SpringBootServletInitializer;
+
 @SpringBootApplication(scanBasePackages = { "com.inno72" })
 @EnableFeignClients
 @EnableEurekaClient
 @EnableCircuitBreaker // 开启熔断
 @EnableConfigurationProperties({ MachineAppBackendProperties.class })
-public class MachineAppBackendApplication extends SpringBootServletInitializer {
+public class MachineCheckAppBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(MachineAppBackendApplication.class, "machine-app-backend", args);
+		new SpringApplicationBuilder(MachineCheckAppBackendApplication.class, "machine-check-app-backend", args);
 	}
 
 	@Override
 	public String setAppNameForLog() {
-		return "machine-app-backend";
+		return "machine-check-app-backend";
 	}
 }
