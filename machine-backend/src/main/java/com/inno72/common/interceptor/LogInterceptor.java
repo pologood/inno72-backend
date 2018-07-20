2576f635a32f0d72b0cb9ed3a26c8ad0a32e8d4d
72cy-0101-01-0013 <wangxiuting@inno72.com>
2018-07-20 19:43:52 +0800

    提交定时 -wxt

--- machine-backend/src/main/java/com/inno72/common/interceptor/LogInterceptor.java
+++ machine-backend/src/main/java/com/inno72/common/interceptor/LogInterceptor.java
@@ -37,7 +37,7 @@ public class LogInterceptor extends HandlerInterceptorAdapter {
 			"/dd/update_call_back", "/dd/initDData", "/dd/token", "/dd/testLogin", "/machine/channel/out/subCount",
 			"/machine/channel/out/get", "/machine/machine/initMachine", "/machine/machine/updateNetStatus",
 			"/machine/machine/updateMachineListNetStatus", "/testMsg", "/project/activityPlan/selectNoPlanMachines",
-            "/machine/locale/selectLocaleByMachineCode", "/supply/channel/status/getChannelErrorDetail","/check/signIn/userExcel"});
+            "/machine/locale/selectLocaleByMachineCode", "/supply/channel/status/getChannelErrorDetail", "/check/signIn/userExcel",
             "/machine/locale/selectLocaleByMachineCode", "/supply/channel/status/getChannelErrorDetail",
             "/check/user/selectAreaMachines"});
 
