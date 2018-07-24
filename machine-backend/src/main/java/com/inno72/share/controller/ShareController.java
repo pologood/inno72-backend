56bde3daba919f6d123c1b9dc249fc8e13db6908
72CY-0101-01-0014 <zhangwenjie@inno72.com>
2018-07-24 10:41:30 +0800

    ‘选择机器最后一级不显示省市区’

---
+++ machine-backend/src/main/java/com/inno72/share/controller/ShareController.java
@@ -0,0 +1,36 @@
+package com.inno72.share.controller;
+
+import com.inno72.common.Result;
+import com.inno72.common.ResultGenerator;
+import com.inno72.share.service.AdminAreaService;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import javax.annotation.Resource;
+
+/**
+* Created by CodeGenerator on 2018/06/29.
+*/
+@RestController
+@RequestMapping("/share")
+@CrossOrigin
+public class ShareController {
+    @Resource
+    private AdminAreaService adminAreaService;
+
+    @RequestMapping(value = "/uploadImage", method = { RequestMethod.POST,  RequestMethod.GET})
+    public @ResponseBody Result<String> uploadImage(@RequestParam(value = "file",required = false) MultipartFile file) {
+    	try {
+    		return null;
+		} catch (Exception e) {
+			return ResultGenerator.genFailResult("操作失败！");
+		}
+    }
+}
