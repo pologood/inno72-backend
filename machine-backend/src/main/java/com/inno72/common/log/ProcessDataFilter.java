82ed3e09940d9b32cac51f126147981f3533ffc8
lzh <lizonghao@inno72.com>
2018-07-26 16:28:41 +0800

    修改日志

--- machine-backend/src/main/java/com/inno72/common/log/ProcessDataFilter.java
+++ machine-backend/src/main/java/com/inno72/common/log/ProcessDataFilter.java
@@ -45,7 +45,7 @@ public class ProcessDataFilter extends OncePerRequestFilter {
 		super.doFilter(req, resp, filterChain);
 		byte[] bytes = resp.getBytes();
 		logger.info("请求地址{}返回值为：{}", request.getServletPath(), new String(bytes));
-
+		response.getOutputStream().write(bytes);
 	}
 
 }
