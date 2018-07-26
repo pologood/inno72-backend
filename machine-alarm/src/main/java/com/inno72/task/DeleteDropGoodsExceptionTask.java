3df943cd32cab26d41ba3ddfa6560849241d95af
72cy-0101-01-0013 <wangxiuting@inno72.com>
2018-07-26 10:55:10 +0800

    提交信息 -wxt

--- machine-alarm/src/main/java/com/inno72/task/DeleteDropGoodsExceptionTask.java
+++ machine-alarm/src/main/java/com/inno72/task/DeleteDropGoodsExceptionTask.java
@@ -31,7 +31,7 @@ public class DeleteDropGoodsExceptionTask {
     public void checkActivityIsExist() {
 
         log.info("每天晚上零点定时删除掉货异常信息表中的数据定时任务，开始执行");
-        List<DropGoodsExceptionInfo> dropGoodsExceptionInfos = mongoTpl.find(new Query(), DropGoodsExceptionInfo.class);
+        List<DropGoodsExceptionInfo> dropGoodsExceptionInfos = mongoTpl.find(new Query(), DropGoodsExceptionInfo.class, "DropGoodsExceptionInfo");
         if (dropGoodsExceptionInfos.size() > 0) {
             mongoTpl.dropCollection("DropGoodsExceptionInfo");
         } else {
