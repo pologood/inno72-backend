15683a7514105ee648b00d6b64c30ee8d04b96e0
72CY-0101-01-0014 <zhangwenjie@inno72.com>
2018-08-02 10:05:21 +0800

    ‘计划排序，状态筛选’

--- machine-backend/src/main/java/com/inno72/project/controller/ActivityPlanController.java
+++ machine-backend/src/main/java/com/inno72/project/controller/ActivityPlanController.java
@@ -1,104 +1,100 @@
 package com.inno72.project.controller;
 
-import com.inno72.common.Result;
-import com.inno72.common.ResultGenerator;
-import com.inno72.project.model.Inno72ActivityPlan;
-import com.inno72.project.service.ActivityPlanService;
-import com.inno72.project.vo.Inno72ActivityPlanVo;
-import com.inno72.project.vo.Inno72AdminAreaVo;
-import com.inno72.common.ResultPages;
-import com.inno72.common.Results;
-import com.inno72.common.StringUtil;
-import com.inno72.project.vo.Inno72NoPlanInfoVo;
-import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-
-import javax.annotation.Resource;
-import java.util.List;
-import java.util.Map;
+import com.inno72.common.Result;
+import com.inno72.common.ResultGenerator;
+import com.inno72.common.ResultPages;
+import com.inno72.common.Results;
+import com.inno72.common.StringUtil;
+import com.inno72.project.model.Inno72ActivityPlan;
+import com.inno72.project.service.ActivityPlanService;
+import com.inno72.project.vo.Inno72ActivityPlanVo;
+import com.inno72.project.vo.Inno72AdminAreaVo;
+import com.inno72.project.vo.Inno72NoPlanInfoVo;
 
 /**
-* Created by CodeGenerator on 2018/07/11.
-*/
+ * Created by CodeGenerator on 2018/07/11.
+ */
 @RestController
 @RequestMapping("/project/activityPlan")
 @CrossOrigin
 public class ActivityPlanController {
-    @Resource
-    private ActivityPlanService activityPlanService;
+	@Resource
+	private ActivityPlanService activityPlanService;
 
-    @RequestMapping(value = "/add", method = { RequestMethod.POST,  RequestMethod.GET})
-    public Result<String> add(@RequestBody Inno72ActivityPlanVo activityPlan) {
-    	try {
-    		return activityPlanService.saveActPlan(activityPlan);
+	@RequestMapping(value = "/add", method = { RequestMethod.POST, RequestMethod.GET })
+	public Result<String> add(@RequestBody Inno72ActivityPlanVo activityPlan) {
+		try {
+			return activityPlanService.saveActPlan(activityPlan);
 		} catch (Exception e) {
 			return ResultGenerator.genFailResult("操作失败！");
 		}
-    }
-    
-    @RequestMapping(value = "/delete", method = { RequestMethod.POST,  RequestMethod.GET})
-    public Result<String> delete(@RequestParam String id) {
-    	try {
-    		return activityPlanService.delById(id);
-		} catch (Exception e) {
-			return ResultGenerator.genFailResult("操作失败！");
-		}
-    }
-    
-    @RequestMapping(value = "/update", method = { RequestMethod.POST,  RequestMethod.GET})
-    public Result<String> update(@RequestBody Inno72ActivityPlanVo activityPlan) {
-    	try {
-    		return activityPlanService.updateModel(activityPlan);
-		} catch (Exception e) {
-			return ResultGenerator.genFailResult("操作失败！");
-		}
-    	
-    }
-    
-    @RequestMapping(value = "/detail", method = { RequestMethod.POST,  RequestMethod.GET})
-    public Result<Inno72ActivityPlan> detail(@RequestParam String id) {
-        Inno72ActivityPlan activityPlan = activityPlanService.findById(id);
-        return ResultGenerator.genSuccessResult(activityPlan);
-    }
-    
-    @RequestMapping(value = "/planMachineDetailList", method = { RequestMethod.POST,  RequestMethod.GET})
-    public Result<List<Map<String, Object>>> planMachineDetailList(@RequestParam String id) {
-    	List<Map<String, Object>> list = activityPlanService.selectPlanMachinDetailList(id);
-        return ResultGenerator.genSuccessResult(list);
-    }
-    
-    @RequestMapping(value = "/list", method = { RequestMethod.POST,  RequestMethod.GET})
-    public ModelAndView list(String code,String startTime,String endTime) {
-        List<Inno72ActivityPlanVo> list = activityPlanService.selectPlanList(code,startTime,endTime);
-        return ResultPages.page(ResultGenerator.genSuccessResult(list));
-    }
-    
+	}
 
-    @RequestMapping(value = "/selectAreaMachines", method = { RequestMethod.POST,  RequestMethod.GET})
-    public Result<List<Inno72AdminAreaVo>> selectMachines(String code,String level,String startTime,String endTime ) {
-   	   	//联动查询
-    	if (StringUtil.isBlank(startTime)||StringUtil.isBlank(endTime)) {
+	@RequestMapping(value = "/delete", method = { RequestMethod.POST, RequestMethod.GET })
+	public Result<String> delete(@RequestParam String id) {
+		try {
+			return activityPlanService.delById(id);
+		} catch (Exception e) {
+			return ResultGenerator.genFailResult("操作失败！");
+		}
+	}
+
+	@RequestMapping(value = "/update", method = { RequestMethod.POST, RequestMethod.GET })
+	public Result<String> update(@RequestBody Inno72ActivityPlanVo activityPlan) {
+		try {
+			return activityPlanService.updateModel(activityPlan);
+		} catch (Exception e) {
+			return ResultGenerator.genFailResult("操作失败！");
+		}
+
+	}
+
+	@RequestMapping(value = "/detail", method = { RequestMethod.POST, RequestMethod.GET })
+	public Result<Inno72ActivityPlan> detail(@RequestParam String id) {
+		Inno72ActivityPlan activityPlan = activityPlanService.findById(id);
+		return ResultGenerator.genSuccessResult(activityPlan);
+	}
+
+	@RequestMapping(value = "/planMachineDetailList", method = { RequestMethod.POST, RequestMethod.GET })
+	public Result<List<Map<String, Object>>> planMachineDetailList(@RequestParam String id) {
+		List<Map<String, Object>> list = activityPlanService.selectPlanMachinDetailList(id);
+		return ResultGenerator.genSuccessResult(list);
+	}
+
+	@RequestMapping(value = "/list", method = { RequestMethod.POST, RequestMethod.GET })
+	public ModelAndView list(String code, String status, String startTime, String endTime) {
+		List<Inno72ActivityPlanVo> list = activityPlanService.selectPlanList(code, status, startTime, endTime);
+		return ResultPages.page(ResultGenerator.genSuccessResult(list));
+	}
+
+	@RequestMapping(value = "/selectAreaMachines", method = { RequestMethod.POST, RequestMethod.GET })
+	public Result<List<Inno72AdminAreaVo>> selectMachines(String code, String level, String startTime, String endTime) {
+		// 联动查询
+		if (StringUtil.isBlank(startTime) || StringUtil.isBlank(endTime)) {
 			return Results.failure("请选择计划时间");
 		}
-        List<Inno72AdminAreaVo> list = activityPlanService.selectAreaMachineList(code,level,startTime,endTime);
-        return ResultGenerator.genSuccessResult(list);
-    }
+		List<Inno72AdminAreaVo> list = activityPlanService.selectAreaMachineList(code, level, startTime, endTime);
+		return ResultGenerator.genSuccessResult(list);
+	}
 
-    @RequestMapping(value = "/selectNoPlanMachines", method = { RequestMethod.POST,  RequestMethod.GET})
-    public Result<List<Inno72NoPlanInfoVo>> selectNoPlanMachines(@RequestParam String taskTime) {
-   	   	//联动查询
-        List<Inno72NoPlanInfoVo> list = activityPlanService.selectNoPlanMachineList(taskTime);
-        return ResultGenerator.genSuccessResult(list);
-    }
+	@RequestMapping(value = "/selectNoPlanMachines", method = { RequestMethod.POST, RequestMethod.GET })
+	public Result<List<Inno72NoPlanInfoVo>> selectNoPlanMachines(@RequestParam String taskTime) {
+		// 联动查询
+		List<Inno72NoPlanInfoVo> list = activityPlanService.selectNoPlanMachineList(taskTime);
+		return ResultGenerator.genSuccessResult(list);
+	}
 
-    
-    
-    
-    
 }
--- machine-backend/src/main/java/com/inno72/project/service/ActivityPlanService.java
+++ machine-backend/src/main/java/com/inno72/project/service/ActivityPlanService.java
@@ -1,15 +1,14 @@
 package com.inno72.project.service;
 
+import java.util.List;
+import java.util.Map;
+
 import com.inno72.common.Result;
 import com.inno72.common.Service;
 import com.inno72.project.model.Inno72ActivityPlan;
 import com.inno72.project.vo.Inno72ActivityPlanVo;
 import com.inno72.project.vo.Inno72AdminAreaVo;
 import com.inno72.project.vo.Inno72NoPlanInfoVo;
-
-import java.util.List;
-import java.util.Map;
-
 
 /**
  * Created by CodeGenerator on 2018/07/11.
@@ -18,9 +17,9 @@ public interface ActivityPlanService extends Service<Inno72ActivityPlan> {
 
 	Result<String> saveActPlan(Inno72ActivityPlanVo activityPlan);
 
-	List<Inno72AdminAreaVo> selectAreaMachineList(String code,String level,String startTime, String endTime);
+	List<Inno72AdminAreaVo> selectAreaMachineList(String code, String level, String startTime, String endTime);
 
-	List<Inno72ActivityPlanVo> selectPlanList(String code, String startTime, String endTime);
+	List<Inno72ActivityPlanVo> selectPlanList(String code, String status, String startTime, String endTime);
 
 	Result<String> delById(String id);
 
@@ -30,11 +29,9 @@ public interface ActivityPlanService extends Service<Inno72ActivityPlan> {
 	 * @describtion 查询没有活动的机器
 	 * @author wxt
 	 * @param taskTime
-	 * */
+	 */
 	List<Inno72NoPlanInfoVo> selectNoPlanMachineList(String taskTime);
 
 	List<Map<String, Object>> selectPlanMachinDetailList(String planId);
-
-
 
 }
