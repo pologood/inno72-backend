3c8e54f448c4725031df14a5853afe9c36764b8b
赵飞龙 <zhaofeilong@inno72.com>
2018-08-03 19:20:39 +0800

    修改商品维度缺货

--- machine-check-app-backend/src/main/java/com/inno72/machine/service/impl/SupplyChannelServiceImpl.java
+++ machine-check-app-backend/src/main/java/com/inno72/machine/service/impl/SupplyChannelServiceImpl.java
@@ -296,6 +296,7 @@ public class SupplyChannelServiceImpl extends AbstractService<Inno72SupplyChanne
                     BigDecimal goodsCountDecimal = new BigDecimal(totalGoodsCount);
                     BigDecimal countDecimal = goodsCountDecimal.divide(volumeCountDecimal,2, RoundingMode.HALF_UP);
                     if(countDecimal.compareTo(new BigDecimal(0.2))<0){
+                        inno72Goods.setLackGoodsCount(totalVolumeCount-totalGoodsCount);
                         inno72Goods.setLackGoodsStatus(1);
                         inno72Goods.setLackGoodsCount(totalVolumeCount-totalGoodsCount);
                         inno72Goods.setSupplyChannelVoList(null);
