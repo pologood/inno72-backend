c45c303fb3f3dac52d231613f5fb15df8a01310c
赵飞龙 <zhaofeilong@inno72.com>
2018-08-07 13:59:00 +0800

    修改APP下载版本地址

--- machine-check-app-backend/src/main/java/com/inno72/check/service/impl/AppVersionServiceImpl.java
+++ machine-check-app-backend/src/main/java/com/inno72/check/service/impl/AppVersionServiceImpl.java
@@ -4,10 +4,7 @@ package com.inno72.check.service.impl;
 import com.inno72.check.mapper.Inno72AppVersionMapper;
 import com.inno72.check.model.Inno72AppVersion;
 import com.inno72.check.service.AppVersionService;
-import com.inno72.common.AbstractService;
-import com.inno72.common.Result;
-import com.inno72.common.ResultGenerator;
-import com.inno72.common.Results;
+import com.inno72.common.*;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -27,6 +24,9 @@ public class AppVersionServiceImpl extends AbstractService<Inno72AppVersion> imp
         Map<String,Object> map = new HashMap<>();
         map.put("appType",inno72AppVersion.getAppType());
         Inno72AppVersion version = inno72AppVersionMapper.selectNowVersion(map);
+        if(version != null){
+            version.setUrl(CommonConstants.DOWNLOAD_APP_PREF+version.getUrl());
+        }
         return ResultGenerator.genSuccessResult(version);
     }
 }
