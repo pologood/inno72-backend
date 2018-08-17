03671d7014e7c8bac725771f24803fb2e215fe45
lzh <lizonghao@inno72.com>
2018-08-17 12:45:07 +0800

    修改

---
+++ machine-monitor-backend/src/main/java/com/inno72/service/AppScreenShotService.java
@@ -0,0 +1,11 @@
+package com.inno72.service;
+import com.inno72.model.Inno72AppScreenShot;
+import com.inno72.common.Service;
+
+
+/**
+ * Created by CodeGenerator on 2018/08/17.
+ */
+public interface AppScreenShotService extends Service<Inno72AppScreenShot> {
+
+}
--- machine-monitor-backend/src/test/java/com/inno72/generator/CodeGenerator.java
+++ machine-monitor-backend/src/test/java/com/inno72/generator/CodeGenerator.java
@@ -60,7 +60,7 @@ public class CodeGenerator {
 	private static final String DATE = new SimpleDateFormat("yyyy/MM/dd").format(new Date());// @date
 
 	public static void main(String[] args) {
-		genCode("inno72_admin_area");
+		genCode("inno72_app_screen_shot");
 		// genCodeByCustomModelName("输入表名","输入自定义Model名称");
 	}
 
