209595df5d00081676c8b63f8b07a6bd785f73a8
赵飞龙 <zhaofeilong@inno72.com>
2018-08-28 17:34:41 +0800

    修改发送push

--- machine-alarm/src/main/java/com/inno72/msgconsumer/RedisReceiver.java
+++ machine-alarm/src/main/java/com/inno72/msgconsumer/RedisReceiver.java
@@ -254,7 +254,7 @@ public class RedisReceiver {
             NumberFormat numberFormat = NumberFormat.getInstance();
             // 设置精确到小数点后2位
             numberFormat.setMaximumFractionDigits(2);
-            String result = numberFormat.format((float) lackNum / (float) sum * 100);
+            String result = numberFormat.format((float) surPlusNum / (float) sum * 100);
             float percent = Float.parseFloat(result);
             log.info("machineDropGoods,percent:{}", percent);
             Map<String, String> params = new HashMap<>();
