68cd4e87c85a090438f577762c21c4a694880269
赵飞龙 <zhaofeilong@inno72.com>
2018-08-29 19:10:12 +0800

    修改发送push

--- machine-alarm/src/main/java/com/inno72/service/impl/SupplyChannelServiceImpl.java
+++ machine-alarm/src/main/java/com/inno72/service/impl/SupplyChannelServiceImpl.java
@@ -4,10 +4,14 @@ import com.inno72.common.AbstractService;
 import com.inno72.mapper.Inno72SupplyChannelMapper;
 import com.inno72.model.Inno72SupplyChannel;
 import com.inno72.service.SupplyChannelService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+@Service
+@Transactional
 public class SupplyChannelServiceImpl extends AbstractService<Inno72SupplyChannel> implements SupplyChannelService {
     @Resource
     private Inno72SupplyChannelMapper inno72SupplyChannelMapper;
