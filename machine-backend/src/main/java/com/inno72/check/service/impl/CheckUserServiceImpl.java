99c4c095f376ae673beca389e2cd215f9e9d6630
72CY-0101-01-0014 <zhangwenjie@inno72.com>
2018-09-03 18:03:54 +0800

    ‘人员显示机器描述’

--- machine-backend/src/main/java/com/inno72/check/service/impl/CheckUserServiceImpl.java
+++ machine-backend/src/main/java/com/inno72/check/service/impl/CheckUserServiceImpl.java
@@ -299,6 +299,8 @@ public class CheckUserServiceImpl extends AbstractService<Inno72CheckUser> imple
 			});
 			String r = "已选择" + machines.size() + "台机器，分别位于:" + pList.toString();
 			u.setRemark(r);
+		} else {
+			u.setRemark("");
 		}
 		if (null != u.getArea() && StringUtil.isNotBlank(u.getArea())) {
 			String area = u.getArea();
