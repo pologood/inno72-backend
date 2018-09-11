e2180b45545d0f1fe63dbf9ccc1deeb06840e02f
lzh <lizonghao@inno72.com>
2018-09-11 17:35:53 +0800

    修改登录接口

--- machine-remote-backend/src/main/java/com/inno72/controller/LoginController.java
+++ machine-remote-backend/src/main/java/com/inno72/controller/LoginController.java
@@ -1,20 +1,23 @@
 package com.inno72.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inno72.common.CommonConstants;
 import com.inno72.common.Result;
 import com.inno72.common.Results;
 import com.inno72.common.utils.StringUtil;
+import com.inno72.util.MapUtil;
 
 @RestController
 public class LoginController {
@@ -22,7 +25,9 @@ public class LoginController {
 	private StringRedisTemplate stringRedisTemplate;
 
 	@RequestMapping(value = "/login", method = { RequestMethod.POST, RequestMethod.GET })
-	public Result<String> login(@RequestParam String userName, @RequestParam String password) {
+	public Result<String> login(@RequestBody(required = false) Map<String, Object> param) {
+		String userName = MapUtil.getParam(param, "userName", String.class);
+		String password = MapUtil.getParam(param, "password", String.class);
 		if (StringUtil.isEmpty(userName) || StringUtil.isEmpty(password)) {
 			return Results.failure("用户名或密码为空");
 		}
@@ -39,16 +44,19 @@ public class LoginController {
 	}
 
 	@RequestMapping(value = "/machineList", method = { RequestMethod.POST, RequestMethod.GET })
-	public Result<List<String>> machineList(@RequestParam(defaultValue = "") String machineCode) {
-		String machinKey = CommonConstants.REDIS_SESSION_PATH + machineCode + "*";
+	public Result<List<Map<String, Object>>> machineList(@RequestBody(required = false) Map<String, Object> param) {
+		String machineCode = MapUtil.getParam(param, "machineCode", String.class);
+		String machinKey = CommonConstants.REDIS_SESSION_PATH + (machineCode == null ? "" : machineCode) + "*";
 		Set<String> keys = stringRedisTemplate.keys(machinKey);
 		if (keys == null || keys.isEmpty()) {
 			return Results.success();
 		}
-		List<String> list = new ArrayList<>();
+		List<Map<String, Object>> list = new ArrayList<>();
 		for (String key : keys) {
 			String m = key.substring(16);
-			list.add(m);
+			Map<String, Object> map = new HashMap<>();
+			map.put("machineCode", m);
+			list.add(map);
 		}
 		return Results.success(list);
 	}
