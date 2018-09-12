ba9482b9b87d497f134263d05a30fdd603922964
赵飞龙 <zhaofeilong@inno72.com>
2018-09-12 11:30:01 +0800

    修改函数

--- machine-backend/src/main/java/com/inno72/share/service/impl/AdminAreaServiceImpl.java
+++ machine-backend/src/main/java/com/inno72/share/service/impl/AdminAreaServiceImpl.java
@@ -4,15 +4,21 @@ package com.inno72.share.service.impl;
 import tk.mybatis.mapper.entity.Condition;
 
 import com.inno72.common.AbstractService;
+import com.inno72.common.Result;
+import com.inno72.common.ResultGenerator;
+import com.inno72.common.Results;
 import com.inno72.common.StringUtil;
 import com.inno72.share.mapper.Inno72AdminAreaMapper;
 import com.inno72.share.model.Inno72AdminArea;
 import com.inno72.share.service.AdminAreaService;
 
+import org.apache.poi.ss.formula.functions.T;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -23,19 +29,76 @@ import javax.annotation.Resource;
 @Service
 @Transactional
 public class AdminAreaServiceImpl extends AbstractService<Inno72AdminArea> implements AdminAreaService {
-    @Resource
-    private Inno72AdminAreaMapper inno72AdminAreaMapper;
+	@Resource
+	private Inno72AdminAreaMapper inno72AdminAreaMapper;
 
 	@Override
 	public List<Inno72AdminArea> getLiset(String code) {
 		Condition condition = new Condition( Inno72AdminArea.class);
-	   	 if (StringUtil.isEmpty(code)) {
-	   		 condition.createCriteria().andCondition("level = 1");
-	   	 }else{
-	   		 condition.createCriteria().andEqualTo("parentCode", code);
-	   	 }
+		if (StringUtil.isEmpty(code)) {
+			condition.createCriteria().andCondition("level = 1");
+		}else{
+			condition.createCriteria().andEqualTo("parentCode", code);
+		}
 		return inno72AdminAreaMapper.selectByCondition(condition);
 	}
-    
+
+	@Override
+	public Result<String> saveArea(Inno72AdminArea adminArea) {
+		String parentCode = adminArea.getParentCode();
+		String name = adminArea.getName();
+		if(StringUtil.isEmpty(parentCode) || StringUtil.isEmpty(name)){
+			return Results.failure("参数有误");
+		}
+		List<Inno72AdminArea> areaList = inno72AdminAreaMapper.select(adminArea);
+		if(areaList != null && areaList.size()>0){
+			return Results.failure("请勿重复添加");
+		}
+		Inno72AdminArea area = inno72AdminAreaMapper.selectMaxByParentCode(parentCode);
+		String before = parentCode.substring(0,4);
+		Integer maxMiddle = Integer.parseInt(area.getCode().substring(4,6));
+		String last = area.getCode().substring(6,9);
+		Integer middle = maxMiddle+1;
+		String code = null;
+		String supply = "0";
+		if(middle<10){
+			code = before+supply+middle+last;
+		}else{
+			code = before+middle+last;
+		}
+		adminArea.setLevel(3);
+		adminArea.setCode(code);
+		adminArea.setProvince(area.getProvince());
+		adminArea.setCity(area.getCity());
+		adminArea.setDistrict(name);
+		inno72AdminAreaMapper.insertSelective(adminArea);
+		return ResultGenerator.genSuccessResult();
+	}
+
+	@Override
+	public List<Inno72AdminArea> findByPage(String code) {
+		int num = StringUtil.getAreaCodeNum(code);
+		Map<String,Object> map = new HashMap<>();
+		map.put("code",code);
+		map.put("num",num);
+		List<Inno72AdminArea> list = inno72AdminAreaMapper.findByPage(map);
+		return list;
+	}
+
+	@Override
+	public Result<String> updateArea(Inno72AdminArea adminArea) {
+		String code = adminArea.getCode();
+		String name = adminArea.getName();
+		if(StringUtil.isEmpty(code) || StringUtil.isEmpty(name)){
+			return Results.failure("参数有误");
+		}
+		Inno72AdminArea area = new Inno72AdminArea();
+		area.setCode(code);
+		area.setName(name);
+		area.setDistrict(name);
+		inno72AdminAreaMapper.updateByPrimaryKeySelective(area);
+		return ResultGenerator.genSuccessResult();
+	}
+
 
 }
