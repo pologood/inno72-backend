65c7814a82439822fdc91ef3b0ac47c1aa03abd7
赵飞龙 <zhaofeilong@inno72.com>
2018-09-26 13:24:40 +0800

    修改

--- machine-change/src/main/java/com/inno72/app/service/impl/AppServiceImpl.java
+++ machine-change/src/main/java/com/inno72/app/service/impl/AppServiceImpl.java
@@ -113,8 +113,8 @@ public class AppServiceImpl extends AbstractService<Inno72App> implements AppSer
 
 	private Result<String> sendMsg(String machineCode, SendMessageBean... beans) {
 //		String url = "http://pre_test.72solo.com:30130/sendMsgToClient/sendMsg";
-		String url = "http://pre_test.72solo.com:9080/sendMsgToClient/sendMsg";//测试
-//		String url = "http://api.monitor.inner.inno72.com:9080/sendMsgToClient/sendMsg";//正式
+//		String url = "http://pre_test.72solo.com:9080/sendMsgToClient/sendMsg";//测试
+		String url = "http://api.monitor.inner.inno72.com:9080/sendMsgToClient/sendMsg";//正式
 //		String url ="http://api.monitor.inner.inno72.com:9080/sendMsgToClient/sendMsg";//预发
 		try {
 			String result = HttpClient.post(url, JSON.toJSONString(beans));
