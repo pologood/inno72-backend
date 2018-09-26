2df7a9c3bc87e8c9cf907063ed869fc03157def9
赵飞龙 <zhaofeilong@inno72.com>
2018-09-26 14:11:36 +0800

    修改日志

--- machine-alarm/src/main/java/com/inno72/msgconsumer/RedisReceiver.java
+++ machine-alarm/src/main/java/com/inno72/msgconsumer/RedisReceiver.java
@@ -152,12 +152,14 @@ public class RedisReceiver {
 						param.put("text",StringUtil.setText(text,active));
 						if(group != null){
 							msgUtil.sendDDTextByGroup("dingding_alarm_common", param, group.getGroupId2(), "machineAlarm-RedisReceiver");
+							text = "缺货报警，提醒方式：钉钉和短信，内容：您好，"+localStr+"，机器编号："+machineCode+","+goodsName+"数量已少于"+surPlusNum+"，请及时补货。";
 							StringUtil.logger(CommonConstants.LOG_TYPE_LACKGOODS,machineCode,text);
 						}
 					}else if(surPlusNum == 10 || surPlusNum == 5){
 						text = "您好，"+machine.getLocaleStr()+"，机器编号："+machineCode+"，"+goodsInfo+"请及时联系巡检人员补货";
 						param.put("text",StringUtil.setText(text,active));
 						if(group != null){
+							text = "缺货报警，提醒方式：钉钉，内容：您好，"+localStr+"，机器编号："+machineCode+","+goodsName+"数量已少于"+surPlusNum+"，请及时补货。";
 							StringUtil.logger(CommonConstants.LOG_TYPE_LACKGOODS,machineCode,text);
 							msgUtil.sendDDTextByGroup("dingding_alarm_common", param, group.getGroupId2(), "machineAlarm-RedisReceiver");
 						}
