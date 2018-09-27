e987450660e8a24f41f986f1a9f8fe711d52e6b1
赵飞龙 <zhaofeilong@inno72.com>
2018-09-27 10:21:05 +0800

    修改切换

--- machine-change/src/main/java/com/inno72/app/controller/AppController.java
+++ machine-change/src/main/java/com/inno72/app/controller/AppController.java
@@ -1,6 +1,7 @@
 package com.inno72.app.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.inno72.app.model.Inno72App;
 import com.inno72.app.service.AppService;
 import com.inno72.common.Result;
@@ -28,9 +31,20 @@ public class AppController {
 	@RequestMapping(value="change")
 	public Result<String> changeApp(String machineId,String appPackageName){
 		Result<String> result = appService.changeApp(machineId,appPackageName);
-		if(result.getCode()==0){
-			result.setMsg("切换APP失败，请检查机器网络");
+		JSONObject obj = JSON.parseObject(result.getData());
+		Object ma = obj.get(machineId);
+		int code = 1;
+		String msg = "切换APP失败，请检查机器网络";
+		if(ma != null){
+			String value = obj.get(machineId).toString();
+			if(value.equals("发送成功")){
+				code = 0;
+				msg = "发送成功";
+			}
 		}
+		result.setData("");
+		result.setCode(code);
+		result.setMsg(msg);
 		return result;
 	}
 }
