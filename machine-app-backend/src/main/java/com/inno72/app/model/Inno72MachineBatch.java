360001966d0b7c5e41c526206405ba0792434e9b
lzh <lizonghao@inno72.com>
2018-10-23 15:22:31 +0800

    yh

--- machine-app-backend/src/main/java/com/inno72/app/model/Inno72MachineBatch.java
+++ machine-app-backend/src/main/java/com/inno72/app/model/Inno72MachineBatch.java
@@ -1,41 +1,126 @@
 package com.inno72.app.model;
 
-import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
-@Table(name = "inno72_machine_batch")
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.inno72.common.datetime.CustomLocalDateTimeSerializer;
+
 public class Inno72MachineBatch {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
 
-    @Column(name = "batch_name")
-    private String batchName;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id;
 
-    /**
-     * @return id
-     */
-    public String getId() {
-        return id;
-    }
+	@Column(name = "batch_name")
+	private String batchName;
 
-    /**
-     * @param id
-     */
-    public void setId(String id) {
-        this.id = id;
-    }
+	@Column(name = "create_id")
+	private String createId;
 
-    /**
-     * @return batch_name
-     */
-    public String getBatchName() {
-        return batchName;
-    }
+	@Column(name = "create_user")
+	private String createUser;
 
-    /**
-     * @param batchName
-     */
-    public void setBatchName(String batchName) {
-        this.batchName = batchName;
-    }
+	@Column(name = "create_time")
+	@JsonSerialize(using = CustomLocalDateTimeSerializer.class)
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@Convert(converter = Jsr310JpaConverters.LocalDateTimeConverter.class)
+	private LocalDateTime createTime;
+
+	@Column(name = "update_id")
+	private String updateId;
+
+	@Column(name = "update_user")
+	private String updateUser;
+
+	@Column(name = "update_time")
+	@JsonSerialize(using = CustomLocalDateTimeSerializer.class)
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@Convert(converter = Jsr310JpaConverters.LocalDateTimeConverter.class)
+	private LocalDateTime updateTime;
+
+	@Transient
+	private List<Inno72MachineBatchDetail> detailList;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getBatchName() {
+		return batchName;
+	}
+
+	public void setBatchName(String batchName) {
+		this.batchName = batchName;
+	}
+
+	public String getCreateId() {
+		return createId;
+	}
+
+	public void setCreateId(String createId) {
+		this.createId = createId;
+	}
+
+	public String getCreateUser() {
+		return createUser;
+	}
+
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+
+	public LocalDateTime getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(LocalDateTime createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getUpdateId() {
+		return updateId;
+	}
+
+	public void setUpdateId(String updateId) {
+		this.updateId = updateId;
+	}
+
+	public String getUpdateUser() {
+		return updateUser;
+	}
+
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
+	}
+
+	public LocalDateTime getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(LocalDateTime updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	public List<Inno72MachineBatchDetail> getDetailList() {
+		return detailList;
+	}
+
+	public void setDetailList(List<Inno72MachineBatchDetail> detailList) {
+		this.detailList = detailList;
+	}
 }
