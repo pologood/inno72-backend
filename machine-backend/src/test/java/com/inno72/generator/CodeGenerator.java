c03019a56acb903a8d9b50a125829de18f9d960b
72CY-0101-01-0014 <zhangwenjie@inno72.com>
2018-10-31 18:30:16 +0800

    ‘商品统计’

--- machine-backend/src/test/java/com/inno72/generator/CodeGenerator.java
+++ machine-backend/src/test/java/com/inno72/generator/CodeGenerator.java
@@ -60,7 +60,7 @@ public class CodeGenerator {
 	private static final String DATE = new SimpleDateFormat("yyyy/MM/dd").format(new Date());// @date
 
 	public static void main(String[] args) {
-		genCode("inno72_interact_machine_time");
+		genCode("inno72_machine_goods");
 		// genCodeByCustomModelName("输入表名","输入自定义Model名称");
 	}
 
