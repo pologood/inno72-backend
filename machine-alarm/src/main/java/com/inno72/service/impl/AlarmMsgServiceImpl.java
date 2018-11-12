acea06f467fd24ea3116dcbc38e8aae4b249aa45
赵飞龙 <zhaofeilong@inno72.com>
2018-11-12 14:12:03 +0800

    修改消息

--- machine-alarm/src/main/java/com/inno72/service/impl/AlarmMsgServiceImpl.java
+++ machine-alarm/src/main/java/com/inno72/service/impl/AlarmMsgServiceImpl.java
@@ -101,11 +101,13 @@ public class AlarmMsgServiceImpl extends AbstractService<Inno72AlarmMsg> impleme
 			androidStr = androidStr.substring(0,androidStr.length()-1);
 			params.put("tags",androidStr);
 			msgUtil.sendPush("push_android_check_app", params, null, appName, title, text);
+			logger.info("按标签发送安卓push，接收者为：",androidStr);
 		}
 		if(StringUtil.isNotEmpty(iosStr)){
 			iosStr = iosStr.substring(0,iosStr.length()-1);
 			params.put("tags",iosStr);
 			msgUtil.sendPush("push_ios_check_app", params, null, appName, title, text);
+			logger.info("按标签发送苹果手机push，接收者为：",iosStr);
 		}
 	}
 }
