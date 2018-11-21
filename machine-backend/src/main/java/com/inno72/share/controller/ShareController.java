40918a72e56b71ae6313b64b2d4ad1d5dbb0fe76
72CY-0101-01-0014 <zhangwenjie@inno72.com>
2018-11-21 16:11:16 +0800

    ‘字典接口’

--- machine-backend/src/main/java/com/inno72/share/controller/ShareController.java
+++ machine-backend/src/main/java/com/inno72/share/controller/ShareController.java
@@ -1,36 +1,48 @@
 package com.inno72.share.controller;
 
-import com.inno72.common.Result;
-import com.inno72.common.ResultGenerator;
-import com.inno72.share.service.ShareService;
+import java.util.List;
+import java.util.Map;
 
-import org.springframework.web.bind.annotation.RequestMethod;
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-
-import javax.annotation.Resource;
+import com.inno72.common.Result;
+import com.inno72.common.ResultGenerator;
+import com.inno72.share.service.DictionaryService;
+import com.inno72.share.service.ShareService;
 
 /**
-* Created by CodeGenerator on 2018/06/29.
-*/
+ * Created by CodeGenerator on 2018/06/29.
+ */
 @RestController
 @RequestMapping("/share")
 @CrossOrigin
 public class ShareController {
-    @Resource
-    private ShareService shareService;
+	@Resource
+	private ShareService shareService;
 
-    @RequestMapping(value = "/uploadImage", method = { RequestMethod.POST,  RequestMethod.GET})
-    public @ResponseBody Result<String> uploadImage(@RequestParam(value = "file",required = false) MultipartFile file,String type) {
-    	try {
-    		return shareService.uploadImage(file,type);
+	@Resource
+	private DictionaryService dictionaryService;
+
+	@RequestMapping(value = "/uploadImage", method = { RequestMethod.POST, RequestMethod.GET })
+	public @ResponseBody Result<String> uploadImage(@RequestParam(value = "file", required = false) MultipartFile file,
+			String type) {
+		try {
+			return shareService.uploadImage(file, type);
 		} catch (Exception e) {
 			return ResultGenerator.genFailResult("操作失败！");
 		}
-    }
+	}
+
+	@RequestMapping(value = "/getBaseDict", method = { RequestMethod.POST, RequestMethod.GET })
+	public Result<Map<String, List<Map<String, String>>>> getBaseDict(String type) {
+		return dictionaryService.getBaseDict(type);
+	}
 }
--- machine-backend/src/main/java/com/inno72/share/service/DictionaryService.java
+++ machine-backend/src/main/java/com/inno72/share/service/DictionaryService.java
@@ -1,5 +1,8 @@
 package com.inno72.share.service;
 
+import java.util.List;
+import java.util.Map;
+
 import com.inno72.common.Result;
 import com.inno72.common.Service;
 import com.inno72.share.model.Inno72Dictionary;
@@ -9,5 +12,5 @@ import com.inno72.share.model.Inno72Dictionary;
  */
 public interface DictionaryService extends Service<Inno72Dictionary> {
 
-	Result getBaseDict(String type);
+	Result<Map<String, List<Map<String, String>>>> getBaseDict(String type);
 }
