18071c48731e9502cd0826e42d9b380181f31f56
xingang <546657868@qq.com>
2018-11-21 12:11:56 +0800

    更新商户ID字段

--- machine-backend/src/main/java/com/inno72/project/vo/Inno72MerchantVo.java
+++ machine-backend/src/main/java/com/inno72/project/vo/Inno72MerchantVo.java
@@ -2,16 +2,14 @@ package com.inno72.project.vo;
 
 import com.inno72.project.model.Inno72Merchant;
 
+import lombok.Data;
+
+@Data
 public class Inno72MerchantVo extends Inno72Merchant{
 	
     private String status;
 
-	public String getStatus() {
-		return status;
-	}
+    private String merchantId;
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
 
 }
