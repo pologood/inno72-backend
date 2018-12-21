2bccc23b43ceb16d0ba5789a6b8ea9a14118f45c
赵飞龙 <zhaofeilong@inno72.com>
2018-12-21 11:07:00 +0800

    修改

--- machine-backend/src/main/java/com/inno72/machine/service/impl/MachineServiceImpl.java
+++ machine-backend/src/main/java/com/inno72/machine/service/impl/MachineServiceImpl.java
@@ -405,17 +405,20 @@ public class MachineServiceImpl extends AbstractService<Inno72Machine> implement
 				MachineStartAppBean bean = new MachineStartAppBean();
 				bean.setAppPackageName(status.getAppPackageName());
 				Inno72App app = appService.findBy("appPackageName", status.getAppPackageName());
-				bean.setAppType(app.getAppType());
-				if (appPackageName.equals(status.getAppPackageName()) || app.getAppType() == 1) {
-					bean.setStartStatus(1);
-				} else {
-					bean.setStartStatus(2);
+				if(app != null){
+					bean.setAppType(app.getAppType());
+					if (appPackageName.equals(status.getAppPackageName()) || app.getAppType() == 1) {
+						bean.setStartStatus(1);
+					} else {
+						bean.setStartStatus(2);
+					}
+					if (appPackageName.equals(status.getAppPackageName())) {
+						name2 = status.getAppName();
+						version2 = status.getVersionName();
+					}
+					sl.add(bean);
 				}
-				if (appPackageName.equals(status.getAppPackageName())) {
-					name2 = status.getAppName();
-					version2 = status.getVersionName();
-				}
-				sl.add(bean);
+
 			}
 			msg.setData(sl);
 			String m = "用户{0}，在erp系统中将运行的App由{1}{2}切换为{3}{4}";
