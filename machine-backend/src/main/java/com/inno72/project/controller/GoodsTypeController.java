8948c021462a566255c246e51ed24cd2527a2195
72CY-0101-01-0014 <zhangwenjie@inno72.com>
2019-01-02 16:36:29 +0800

    ‘商品’

--- machine-backend/src/main/java/com/inno72/project/controller/GoodsTypeController.java
+++ machine-backend/src/main/java/com/inno72/project/controller/GoodsTypeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,7 @@ import com.inno72.project.service.GoodsTypeService;
  */
 @RestController
 @RequestMapping("/goods/type")
+@CrossOrigin
 public class GoodsTypeController {
 	@Resource
 	private GoodsTypeService goodsTypeService;
