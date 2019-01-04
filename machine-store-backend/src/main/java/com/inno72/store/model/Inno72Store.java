3ce8b67c8a68d37ee20b0c322892a33aac51cf88
72CY-0101-01-0014 <zhangwenjie@inno72.com>
2019-01-04 11:20:28 +0800

    ‘添加仓库’

--- machine-store-backend/src/main/java/com/inno72/store/model/Inno72Store.java
+++ machine-store-backend/src/main/java/com/inno72/store/model/Inno72Store.java
@@ -30,7 +30,7 @@ public class Inno72Store {
 	/**
 	 * 仓库类型:0 中心仓，1分仓
 	 */
-	private String type;
+	private Integer type;
 
 	/**
 	 * 区域ID
@@ -146,7 +146,7 @@ public class Inno72Store {
 	 *
 	 * @return type - 仓库类型:0 中心仓，1分仓
 	 */
-	public String getType() {
+	public Integer getType() {
 		return type;
 	}
 
@@ -156,7 +156,7 @@ public class Inno72Store {
 	 * @param type
 	 *            仓库类型:0 中心仓，1分仓
 	 */
-	public void setType(String type) {
+	public void setType(Integer type) {
 		this.type = type;
 	}
 
--- machine-store-backend/src/main/java/com/inno72/store/service/impl/StoreServiceImpl.java
+++ machine-store-backend/src/main/java/com/inno72/store/service/impl/StoreServiceImpl.java
@@ -1,14 +1,25 @@
 package com.inno72.store.service.impl;
 
-import com.inno72.store.mapper.Inno72StoreMapper;
-import com.inno72.store.model.Inno72Store;
-import com.inno72.store.service.StoreService;
-import com.inno72.common.AbstractService;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.inno72.common.AbstractService;
+import com.inno72.common.Result;
+import com.inno72.common.Results;
+import com.inno72.common.SessionData;
+import com.inno72.common.SessionUtil;
+import com.inno72.common.StringUtil;
+import com.inno72.store.mapper.Inno72StoreMapper;
+import com.inno72.store.model.Inno72Store;
+import com.inno72.store.model.Inno72Storekeeper;
+import com.inno72.store.service.StoreService;
 
 /**
  * Created by CodeGenerator on 2018/12/28.
@@ -16,7 +27,51 @@ import javax.annotation.Resource;
 @Service
 @Transactional
 public class StoreServiceImpl extends AbstractService<Inno72Store> implements StoreService {
-    @Resource
-    private Inno72StoreMapper inno72StoreMapper;
+
+	private static Logger logger = LoggerFactory.getLogger(StoreServiceImpl.class);
+
+	@Resource
+	private Inno72StoreMapper inno72StoreMapper;
+
+	@Override
+	public Result<Object> saveModel(Inno72Store model) {
+
+		try {
+			SessionData session = SessionUtil.sessionData.get();
+			Inno72Storekeeper mUser = Optional.ofNullable(session).map(SessionData::getStorekeeper).orElse(null);
+			if (mUser == null) {
+				logger.info("登陆用户为空");
+				return Results.failure("未找到用户登录信息");
+			}
+			String mUserId = Optional.ofNullable(mUser).map(Inno72Storekeeper::getId).orElse(null);
+			model.setId(StringUtil.getUUID());
+			model.setCreateId(mUserId);
+			model.setUpdateId(mUserId);
+			model.setCreateTime(LocalDateTime.now());
+			model.setUpdateTime(LocalDateTime.now());
+
+			if (StringUtil.isBlank(model.getName())) {
+				logger.info("请填写仓库名称");
+				return Results.failure("请填写仓库名称");
+			}
+			if (StringUtil.isBlank(model.getAreaCode())) {
+				logger.info("请选择地区位置");
+				return Results.failure("请选择地区位置");
+			}
+			if (StringUtil.isBlank(model.getArea())) {
+				logger.info("请填写详细地址");
+				return Results.failure("请填写详细地址");
+			}
+
+			inno72StoreMapper.insert(model);
+
+			return Results.warn("操作成功", 0, model.getId());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.info(e.getMessage());
+			return Results.failure("操作失败");
+		}
+	}
 
 }
