85c71d8915b5847a6ce03fd10b1b288f09d78c6b
lzh <lizonghao@inno72.com>
2019-01-07 14:47:02 +0800

    修改socket

--- machine-monitor-backend/src/main/java/com/inno72/controller/SendMsgToClientController.java
+++ machine-monitor-backend/src/main/java/com/inno72/controller/SendMsgToClientController.java
@@ -1,6 +1,5 @@
 package com.inno72.controller;
 
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,21 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
-import com.inno72.common.CommonConstants;
+import com.inno72.common.MachineMonitorBackendProperties;
 import com.inno72.common.Result;
 import com.inno72.common.ResultGenerator;
 import com.inno72.common.utils.StringUtil;
-import com.inno72.model.AppVersion;
-import com.inno72.model.Inno72AppMsg;
-import com.inno72.model.MachineAppStatus;
-import com.inno72.model.MessageBean;
 import com.inno72.model.SendMessageBean;
+import com.inno72.plugin.http.HttpClient;
 import com.inno72.redis.IRedisUtil;
-import com.inno72.service.AppMsgService;
 import com.inno72.service.SocketService;
 import com.inno72.util.AesUtils;
 import com.inno72.util.GZIPUtil;
+import com.inno72.vo.AppMsgVo;
 
 @RestController
 @RequestMapping("/sendMsgToClient")
@@ -42,31 +37,37 @@ public class SendMsgToClientController {
 	private IRedisUtil redisUtil;
 
 	@Autowired
-	private AppMsgService appMsgService;
-	@Autowired
 	private SocketService socketService;
 	@Autowired
 	private MongoOperations mongoTpl;
 
+	@Autowired
+	private MachineMonitorBackendProperties machineMonitorBackendProperties;
+
 	@RequestMapping(value = "/sendMsg", method = { RequestMethod.POST, RequestMethod.GET })
 	public Result<Map<String, Object>> sendMsg(@RequestBody SendMessageBean... msgs) {
 		Map<String, Object> map = new HashMap<>();
+		String url = machineMonitorBackendProperties.get("sendMsgUrl");
 		for (SendMessageBean msg : msgs) {
 			logger.info("客户端发送消息：{}", JSON.toJSONString(msg));
 			String result = GZIPUtil.compress(AesUtils.encrypt(JSON.toJSONString(msg)));
-			String machinKey = CommonConstants.REDIS_SESSION_PATH + msg.getMachineId();
-			String sessionId = redisUtil.get(machinKey);
-			if (!com.inno72.common.utils.StringUtil.isEmpty(sessionId)) {
-				Inno72AppMsg msg1 = new Inno72AppMsg();
-				msg1.setId(StringUtil.uuid());
-				msg1.setCreateTime(LocalDateTime.now());
-				msg1.setMachineCode(msg.getMachineId());
-				msg1.setContent(result);
-				msg1.setStatus(0);
-				msg1.setMsgType(1);
-				appMsgService.save(msg1);
-				map.put(msg.getMachineId(), "发送成功");
-			} else {
+			AppMsgVo vo = new AppMsgVo();
+			vo.setData(result);
+			vo.setIsQueue(1);
+			vo.setTargetCode(msg.getMachineId());
+			vo.setTargetType("machine");
+			try {
+				String r = HttpClient.post(url, JSON.toJSONString(vo));
+				if (!StringUtil.isEmpty(r)) {
+					JSONObject $_result = JSON.parseObject(result);
+					if ($_result.getInteger("code") == 0) {
+						map.put(msg.getMachineId(), "发送成功");
+					} else {
+						map.put(msg.getMachineId(), "发送失败");
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 				map.put(msg.getMachineId(), "发送失败");
 			}
 		}
@@ -78,51 +79,28 @@ public class SendMsgToClientController {
 		Map<String, Object> map = new HashMap<>();
 		logger.info("客户端发送消息：{}", JSON.toJSONString(param));
 		String result = GZIPUtil.compress(AesUtils.encrypt(JSON.toJSONString(param.get("msg"))));
+		String url = machineMonitorBackendProperties.get("sendMsgUrl");
 		String machineCode = (String) param.get("machineCode");
-		String machinKey = CommonConstants.REDIS_SESSION_PATH + machineCode;
-		String sessionId = redisUtil.get(machinKey);
-		if (!com.inno72.common.utils.StringUtil.isEmpty(sessionId)) {
-			Inno72AppMsg msg1 = new Inno72AppMsg();
-			msg1.setId(StringUtil.uuid());
-			msg1.setCreateTime(LocalDateTime.now());
-			msg1.setMachineCode(machineCode);
-			msg1.setContent(result);
-			msg1.setStatus(0);
-			msg1.setMsgType(3);
-			appMsgService.save(msg1);
-			map.put(machineCode, "发送成功");
-		} else {
+		AppMsgVo vo = new AppMsgVo();
+		vo.setData(result);
+		vo.setIsQueue(1);
+		vo.setTargetCode(machineCode);
+		vo.setTargetType("machine");
+		try {
+			String r = HttpClient.post(url, JSON.toJSONString(vo));
+			if (!StringUtil.isEmpty(r)) {
+				JSONObject $_result = JSON.parseObject(result);
+				if ($_result.getInteger("code") == 0) {
+					map.put(machineCode, "发送成功");
+				} else {
+					map.put(machineCode, "发送失败");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 			map.put(machineCode, "发送失败");
 		}
 		return ResultGenerator.genSuccessResult(map);
 	}
 
-	@RequestMapping(value = "/test", method = { RequestMethod.POST, RequestMethod.GET })
-	public void test() {
-		String s = "{\"data\":{\"machineId\":\"18333245\",\"status\":[{\"appName\":\"\",\"appPackageName\":\"com.inno72.app\",\"appStatus\":0,\"versionCode\":-1},{\"appName\":\"72监控APP\",\"appPackageName\":\"com.inno72.monitorapp\",\"appStatus\":2,\"versionCode\":12,\"versionName\":\"1.1.2\"},{\"appName\":\"72安装器\",\"appPackageName\":\"com.inno72.installer\",\"appStatus\":2,\"versionCode\":4,\"versionName\":\"1.0.3\"},{\"appName\":\"72管理\",\"appPackageName\":\"com.inno72.detection\",\"appStatus\":2,\"versionCode\":6,\"versionName\":\"1.0.5\"},{\"appName\":\"72数据中心\",\"appPackageName\":\"com.inno72.dc\",\"appStatus\":2,\"versionCode\":5,\"versionName\":\"1.0.4\"},{\"appName\":\"壶中界\",\"appPackageName\":\"com.tmall.hudong.worldinpot\",\"appStatus\":2,\"versionCode\":4,\"versionName\":\"0.0.4\"},{\"appName\":\"72上传\",\"appPackageName\":\"com.inno72.upload\",\"appStatus\":0,\"versionCode\":3,\"versionName\":\"1.0.2\"},{\"appName\":\"\",\"appPackageName\":\"com.inno72.demons\",\"appStatus\":0,\"versionCode\":-1},{\"appName\":\"点仙界\",\"appPackageName\":\"com.inno72.pot.limit\",\"appStatus\":1,\"versionCode\":2,\"versionName\":\"1.0.1\"}]},\"eventType\":1,\"subEventType\":2}";
-		MessageBean<MachineAppStatus> appStatus = JSONObject.parseObject(s,
-				new TypeReference<MessageBean<MachineAppStatus>>() {
-				});
-		MachineAppStatus apps = appStatus.getData();
-		apps.setCreateTime(LocalDateTime.now());
-		socketService.checkApp(apps);
-	}
-
-	@RequestMapping(value = "/test1", method = { RequestMethod.POST, RequestMethod.GET })
-	public void test1() {
-		AppVersion app = new AppVersion();
-		app.setAppName("72App");
-		app.setDownloadUrl("http://inno72.oss.72solo.com/apk/prod/prod_72app1.1.1.apk");
-		app.setAppPackageName("com.inno72.app");
-		app.setIsUpgrade(2);
-		app.setAppVersionCode(5);
-		app.setSeq(99);
-		app.setStatus(1);
-		mongoTpl.save(app);
-	}
-public static void main(String[] args) {
-	String a="H4sIAAAAAAAAABWUywHDMAhDVzKY7zjGmP1HiHJqmroGniRyDm1ecWKUypm69Wr3Dpq+SdR19lmbrxzDu+zN9HqRmtt25rjme0LoztliR3e+kMxrdOh52Y06JTytZPH0WZxSOpr3r2nnxHkyTGcOi89pTrwUf2/tW0IrInztQ7hq7sa972TNqNB5fsvsbdQWFS50I6mZ2XfnfVLxunYY+2jfRXP9aOfhlLX2zvNOmIeet3m/dGWRqAGQEg3Mhcdr6kbsOazE7c+YdmS+1ccqTBbnfpTobqOELOF7dDDFS+mVbNFVGRqWtXY317ETHAKCTVZkcny9stXhqi2i1mei3bjuHdO5LyI3OHQUG2/2eyxneqvcA7wrz3FoFE+495BCged5rqKXIJ3iuQAnI0TocptcfD7yuw/UxuOeix7ZGSNZV1/hvPmY1w0/AII6kMRrnZ+fgr3ZFe1NETqgrHdcqsBADRyXrq2nGtyYk+PgD13GEAeeI8IDtHhksM2lXDkJnC+hYr/I5dX89uN1Nqbyd/jukkmegWS5McswlEmYVh/IbC3//Ynht0/w7N9pVRRz3l/f1uNc5/5Ud+k67SslRphrBdSG7rW9T9R7FdSkCToQ0d7cGdAc0fRtuYhlAv10LPjWnrbRrjWwrv0MhpYtYCFldf5Z9iuNYeTInTcCgyrUi4n1JfwLAtZAxbK2+ZK0pBkizKli02zZsxCPlQ+CmgNKehyKd6ffwJVtHRmrWwroaLyRpWc7R2TVuENoNhnkvP6rffsiiIgo34Xewx/0amFNUwk+0q62QxBgxdEicBAqfNl++0JOoWf1KnMv54KPcRZgYv03wlshY1gvL1UKqa/clElwbFAgJpf4pNkUKiLPsAGgi98gGJUg2m1x5Pwd3Dy2CQE/Bk/FQhP/JsAKaaF+BMtrVB/UxQ/4t65EFG5hCyxUh/5KA7XwqzGihwUxWEG3/J/ZMIY+nLrbsN7EEgIxtiK+i0i+8lg4REzWbcOVnmBx4M/GcsEprcYK9etQF0vp9OQHMteKTmAFAAA=";
-			String aa = GZIPUtil.uncompress(AesUtils.decrypt(a));
-			System.out.println(aa);
-}
 }
