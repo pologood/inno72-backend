6de9a5ba79322c0a92be3864936309fcb24c16ba
72CY-0101-01-0014 <zhangwenjie@inno72.com>
2019-01-08 14:51:02 +0800

    发货单

--- machine-store-backend/src/main/java/com/inno72/store/service/StoreOrderService.java
+++ machine-store-backend/src/main/java/com/inno72/store/service/StoreOrderService.java
@@ -1,11 +1,14 @@
 package com.inno72.store.service;
-import com.inno72.store.model.Inno72StoreOrder;
-import com.inno72.common.Service;
 
+import com.inno72.common.Result;
+import com.inno72.common.Service;
+import com.inno72.store.model.Inno72StoreOrder;
 
 /**
  * Created by CodeGenerator on 2018/12/28.
  */
 public interface StoreOrderService extends Service<Inno72StoreOrder> {
+
+	Result<Object> saveModel(Inno72StoreOrder model);
 
 }
