a9320a2b0236da55cc64fe687e6ed3b2198823ce
72CY-0101-01-0014 <zhangwenjie@inno72.com>
2019-01-14 15:23:14 +0800

    ‘更新库存’

--- machine-store-backend/src/main/java/com/inno72/store/mapper/Inno72CheckGoodsDetailMapper.java
+++ machine-store-backend/src/main/java/com/inno72/store/mapper/Inno72CheckGoodsDetailMapper.java
@@ -3,5 +3,6 @@ package com.inno72.store.mapper;
 import com.inno72.common.Mapper;
 import com.inno72.store.model.Inno72CheckGoodsDetail;
 
+@org.apache.ibatis.annotations.Mapper
 public interface Inno72CheckGoodsDetailMapper extends Mapper<Inno72CheckGoodsDetail> {
 }
