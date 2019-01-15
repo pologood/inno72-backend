e07f77a2f571730736b6e169a370d3dd3a311704
xingang <546657868@qq.com>
2019-01-15 11:59:26 +0800

    修改字段名

--- machine-backend/src/main/java/com/inno72/project/mapper/Inno72ActivityIndexMapper.java
+++ machine-backend/src/main/java/com/inno72/project/mapper/Inno72ActivityIndexMapper.java
@@ -10,6 +10,4 @@ import com.inno72.project.model.Inno72ActivityIndex;
 
 public interface Inno72ActivityIndexMapper extends Mapper<Inno72ActivityIndex> {
 	List<Inno72ActivityIndex> selectIndex(@Param("merchantId") String merchantId,@Param("activityId") String activityId);
-
-	List<Inno72ActivityIndex> selectByActIdAndIndexType(Map<String, String> param);
 }
