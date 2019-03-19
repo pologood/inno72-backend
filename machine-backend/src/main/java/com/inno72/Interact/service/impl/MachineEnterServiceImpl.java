80256568bee40b137520b04eacab3455926b15a1
72CY-0101-01-0014 <zhangwenjie@inno72.com>
2019-03-19 16:20:07 +0800

    ‘入驻’

--- machine-backend/src/main/java/com/inno72/Interact/service/impl/MachineEnterServiceImpl.java
+++ machine-backend/src/main/java/com/inno72/Interact/service/impl/MachineEnterServiceImpl.java
@@ -16,6 +16,7 @@ import com.alipay.api.request.AntMerchantExpandAutomatApplyUploadRequest;
 import com.alipay.api.response.AntMerchantExpandAutomatApplyUploadResponse;
 import com.inno72.Interact.mapper.Inno72MachineEnterMapper;
 import com.inno72.Interact.service.MachineEnterService;
+import com.inno72.Interact.vo.AlipayAutomat;
 import com.inno72.common.DateUtil;
 import com.inno72.common.Result;
 import com.inno72.common.Results;
@@ -70,66 +71,44 @@ public class MachineEnterServiceImpl implements MachineEnterService {
 			String cityCode = inno72AlipayArea.getParentCode();
 			String provinceCode = getParentCode(areaCode, 1);
 
-			Map<String, Object> param = new HashMap<String, Object>();
-			param.put("terminal_id", machineAddress.get("machineCode"));
-			param.put("product_user_id", alipayUserId);
-			param.put("merchant_user_id", alipayUserId);
-			param.put("machine_type", "AUTOMAT");
-			param.put("machine_cooperation_type", "COOPERATION_EXCLUSIVE");
-			param.put("machine_delivery_date", DateUtil.nowStr());
-			param.put("machine_name", "点72");
-			param.put("merchant_user_type", "ALIPAY_MERCHANT");
+			AlipayAutomat automat = new AlipayAutomat();
 
-			Map<String, Object> associate = new HashMap<String, Object>();
+			automat.setTerminal_id(machineAddress.get("machineCode"));
+			automat.setProduct_user_id(alipayUserId);
+			automat.setMerchant_user_id(alipayUserId);
+			automat.setMachine_type("AUTOMAT");
+			automat.setMachine_cooperation_type("COOPERATION_EXCLUSIVE");
+			automat.setMachine_delivery_date(DateUtil.nowStr());
+			automat.setMachine_name("点72");
+
+			Map<String, String> associate = new HashMap<String, String>();
 			associate.put("associate_type ", "DISTRIBUTORS");
 			associate.put("associate_user_id", alipayUserId);
 
-			Map<String, Object> deliveryAddress = new HashMap<String, Object>();
-			deliveryAddress.put("province_code", provinceCode);
-			deliveryAddress.put("city_code", cityCode);
-			deliveryAddress.put("area_code", areaCode);
-			deliveryAddress.put("machine_address", address);
+			Map<String, String> areaAddress = new HashMap<String, String>();
+			areaAddress.put("province_code", provinceCode);
+			areaAddress.put("city_code", cityCode);
+			areaAddress.put("area_code", areaCode);
+			areaAddress.put("machine_address", address);
 
-			/*
-			 * deliveryAddress.put("province_code", "110000");
-			 * deliveryAddress.put("city_code", "110100");
-			 * deliveryAddress.put("area_code", "110105");
-			 * deliveryAddress.put("machine_address", "骏豪朝阳公园广场");
-			 */
+			automat.setDelivery_address(areaAddress);
+			automat.setPoint_position(areaAddress);
+			automat.setMerchant_user_type("ALIPAY_MERCHANT");
+			// p.setAssociate(associate);
 
-			Map<String, Object> pointPosition = new HashMap<String, Object>();
-			pointPosition.put("province_code", provinceCode);
-			pointPosition.put("city_code", cityCode);
-			pointPosition.put("area_code", areaCode);
-			pointPosition.put("machine_address", address);
-
-			/*
-			 * pointPosition.put("province_code", "110000");
-			 * pointPosition.put("city_code", "110100");
-			 * pointPosition.put("area_code", "110105");
-			 * pointPosition.put("machine_address", "骏豪朝阳公园广场");
-			 */
-
-			Map<String, Object> scene = new HashMap<String, Object>();
+			Map<String, String> scene = new HashMap<String, String>();
 			scene.put("level_1", "MALL");
 			scene.put("level_2", "001");
-
-			param.put("delivery_address", deliveryAddress);
-			param.put("point_position", pointPosition);
-			param.put("associate", associate);
-			param.put("scene", scene);
-
-			System.out.println("入驻参数：" + JsonUtil.toJson(param));
-
-			request.setBizContent(JsonUtil.toJson(param));
+			automat.setScene(scene);
+			logger.info("入驻参数：" + JsonUtil.toJson(automat));
 
 			AntMerchantExpandAutomatApplyUploadResponse response = alipayClient.execute(request);
 			if (response.isSuccess()) {
 				logger.info("入驻成功：" + response.getAlipayTerminalId());
 				return Results.warn("入驻成功", 0, response.getAlipayTerminalId());
 			} else {
-				logger.info("入驻失败：" + response.getCode() + response.getMsg());
-				return Results.failure(response.getMsg());
+				logger.info("入驻失败：" + response.getCode() + response.getSubMsg());
+				return Results.failure(response.getSubMsg());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
